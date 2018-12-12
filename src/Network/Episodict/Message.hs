7ff7d77cb1ed4ba2abd6b0008b97591a67a23f61
Eugene Butler <eugene@eugene4.com>
2018-12-11 22:11:04 -0500

    actually do the thing

--- src/Network/Episodict/Message.hs
+++ src/Network/Episodict/Message.hs
@@ -7,10 +7,17 @@ import GHC.Generics (Generic)
 import qualified Data.Map.Strict as M
 
 data Message a b = Message {
-  insertedMap :: M.Map a b,
-  deletedMap :: M.Map a (),
-  t :: Integer,
+  viewMap :: (M.Map a (ViewValue b)),
+  t :: (M.Map String Integer),
   nodeId :: String
 } deriving (Show, Generic)
 
 instance (Binary a, Binary b) => Binary (Message a b)
+
+data ViewValue b = ViewValue {
+  creator :: String,
+  time :: Integer,
+  value :: b
+} deriving (Show, Generic)
+
+instance (Binary b) => Binary (ViewValue b)
--- src/Network/Episodict/Node.hs
+++ src/Network/Episodict/Node.hs
@@ -7,7 +7,7 @@ import Control.Concurrent
 import qualified Data.Map.Strict as M
 import Control.Monad (forever, mapM_)
 import Control.Concurrent (ThreadId, forkIO)
-import Control.Concurrent.STM (TQueue, TMVar, TVar, retry, modifyTVar, readTVar, newTVarIO, newTQueueIO, newEmptyTMVarIO, atomically, putTMVar, writeTQueue, tryReadTQueue)
+import Control.Concurrent.STM (TQueue, TMVar, TVar, writeTVar, readTVarIO, retry, modifyTVar, readTVar, newTVarIO, newTQueueIO, newEmptyTMVarIO, atomically, putTMVar, writeTQueue, tryReadTQueue)
 import Network.Socket hiding (recv)
 
 import Network.Episodict.Connection
@@ -16,41 +16,49 @@ import Network.Episodict.Message
 data Node a b = Node {
   nId :: String,
   peers :: TVar (M.Map String (Connection a b)),
-  inserted :: TVar (M.Map a b),
-  deleted :: TVar (M.Map a ()),
+  view :: TVar (M.Map a (ViewValue b)),
   clock :: TVar Integer,
+  nodeTimes :: TVar (M.Map String Integer),
   myTID :: TMVar ThreadId
 }
 
+-- R1. We assume that there is at most one occurrence of the operation
+-- INSERT(x) for each element x, so that once an element has been deleted
+-- from the set, it can never again be reinserted
+-- (Hrm. I don't know about this. Not sure if there's any reason I can't support multi-insert w/o delete,
+-- even if it makes some of the logic more complicated)
+-- TODO: Do some insert mandatory dedupe thing later.
 insert :: (Ord a) => Node a b -> a -> b -> IO ()
 insert n k v = atomically $ do
-  modifyTVar (inserted n) (M.insert k v)
-  modifyTVar (clock n) (+ 1)
-  return ()
+  -- t[i] := clock[i]
+  theTime <- getClock n
+  -- cre[x] := i, T[x] := t[i](i)
+  let viewValue = ViewValue (nId n) theTime v
+  -- V[i] := V U {x}
+  modifyTVar (view n) (M.insert k viewValue)
   -- TODO: auto-send?
-  -- return $ n { inserted = newInserted, clock = newClock }
 
+-- R2. DELETE(x) is only legal at node j if x is currently in j's view
 delete :: (Ord a) => Node a b -> a -> IO ()
 delete n k = atomically $ do
-  modifyTVar (deleted n) (M.insert k ())
-  return ()
+  viewMap <- readTVar $ view n
+  case (M.lookup k viewMap) of
+    Just _ -> do
+      modifyTVar (view n) (M.delete k)
+    Nothing -> return () -- Throw error?
   -- TODO: auto-send?
-  -- return $ n { deleted = newDeleted }
 
-list :: (Ord a) => Node a b -> IO (M.Map a b)
-list n = atomically $ do
-  ins <- readTVar (inserted n)
-  del <- readTVar (deleted n)
-  return $ M.difference ins del
+list :: (Ord a) => Node a b -> IO (M.Map a (ViewValue b))
+list n = atomically $ readTVar (view n)
 
 newNode :: (Show a, Show b, Ord a) => String -> IO (Node a b)
 newNode name = do
-  ins <- newTVarIO M.empty
-  del <- newTVarIO M.empty
   cl <- newTVarIO 0
   p <- newTVarIO M.empty
+  v <- newTVarIO M.empty
+  t <- newTVarIO $ M.fromList [(name, 0)]
   tid <- newEmptyTMVarIO
-  let n = Node name p ins del cl tid
+  let n = Node name p v cl t tid
   nodeReceive n
   return n
 
@@ -78,17 +86,39 @@ send n = do
 receive :: (Ord a) => Node a b -> Message a b -> IO (Node a b)
 receive n msg = do
   atomically $ do
-    modifyTVar (inserted n) (M.union (insertedMap msg))
-    modifyTVar (deleted n) (M.union (deletedMap msg))
-  -- TODO: actually reconcile the dicts
+    localView <- readTVar $ view n
+    localTimeVector <- readTVar $ nodeTimes n
+    let otherView = viewMap msg
+    let comboView = M.union localView otherView
+    let remoteTimeVector = t msg
+    let theNewView = M.foldrWithKey (\k cVV newView -> case ((not $ shouldDelete localView localTimeVector k cVV) && (not $ shouldDelete otherView remoteTimeVector k cVV)) of
+                                        True -> (M.insert k cVV newView)
+                                        False -> newView
+                   ) M.empty comboView
+    let theNewTimes = M.foldrWithKey (\k kTime newTimes -> case (M.lookup k newTimes) of
+          Nothing -> M.insert k kTime newTimes
+          Just otherTime -> case (kTime < otherTime) of
+            True -> M.insert k otherTime newTimes
+            False -> M.insert k kTime newTimes
+                                     ) remoteTimeVector localTimeVector
+    writeTVar (view n) theNewView
+    writeTVar (nodeTimes n) theNewTimes
   return n
+
+-- del(V, t, x) iff [x nin V and T[x] <= t(cre[x])]
+shouldDelete :: (Ord a) => (M.Map a (ViewValue b)) -> (M.Map String Integer) ->  a -> (ViewValue b) -> Bool
+shouldDelete localView timeVector k vV = case (M.lookup k localView) of
+    Just _ -> False
+    Nothing -> case (M.lookup (creator vV) timeVector) of
+      Nothing -> False -- Uhhhhhh...
+      Just creatorTime -> ((time vV) <= creatorTime)
 
 createMessage :: (Ord a) => Node a b -> IO (Message a b)
 createMessage n = atomically $ do
-  ins <- readTVar (inserted n)
-  del <- readTVar (deleted n)
-  cl <- readTVar (clock n)
-  return $ Message ins del (cl) (nId n)
+  v <- readTVar (view n)
+  times <- readTVar $ nodeTimes n
+  cl <- getClock n
+  return $ Message v (M.insert (nId n) cl times) (nId n)
 
 sendMessage :: (Show a, Show b) => Message a b -> Connection a b -> IO ()
 sendMessage message conn = do
@@ -102,24 +132,54 @@ connectNodes n1 n2 = do
     modifyTVar (peers n1) (M.insert (nId n2) c1)
     modifyTVar (peers n2) (M.insert (nId n1) c2)
 
+getClock n = do
+  modifyTVar (clock n) (+1)
+  newClock <- readTVar $ clock n
+  modifyTVar (nodeTimes n) (M.insert (nId n) newClock)
+  return newClock
+
 testLocal :: IO ()
 testLocal = do
   node1 <- newNode "node1"
   node2 <- newNode "node2"
+  node3 <- newNode "node3"
   connectNodes node1 node2
+  connectNodes node2 node3
+  threadDelay 1000
   Network.Episodict.Node.insert node1 "a" "b"
   Network.Episodict.Node.send node1
+  threadDelay 1000
   Network.Episodict.Node.insert node2 "c" "3"
   Network.Episodict.Node.send node2
+  threadDelay 1000
   Network.Episodict.Node.delete node1 "a"
   Network.Episodict.Node.send node1
+  threadDelay 1000
+  Network.Episodict.Node.delete node3 "c"
+  Network.Episodict.Node.send node3
+  threadDelay 1000
+  Network.Episodict.Node.send node2
+  threadDelay 1000
+  Network.Episodict.Node.insert node3 "d" "4"
+  Network.Episodict.Node.send node3
+  threadDelay 1000
+  Network.Episodict.Node.send node2
   threadDelay 1000
   l1 <- list node1
   print "list of node 1"
   print l1
+  times <- readTVarIO $ nodeTimes node1
+  print times
   l2 <- list node2
   print "list of node 2"
   print l2
+  times <- readTVarIO $ nodeTimes node2
+  print times
+  l3 <- list node3
+  print "list of node 3"
+  print l3
+  times <- readTVarIO $ nodeTimes node3
+  print times
 
 testSocket :: IO ()
 testSocket = do
